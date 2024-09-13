e77c9217a4c90ae1f50f2449005ee10a0102e76c
sheerapi <luciano90lagos@gmail.com>
2024-09-12 22:31:47 -0300

    Made system manager

---
+++ espresso/utils/Demangle.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <typeinfo>
+
+#ifdef __GNUG__
+#	include <cstdlib>
+#	include <cxxabi.h>
+#	include <memory>
+#endif
+
+namespace Espresso::Internals
+{
+	template <typename T> auto demangle() -> std::string
+	{
+		const auto* name = typeid(T).name();
+#ifdef __GNUG__
+		int status;
+
+		std::unique_ptr<char, void (*)(void*)> res{
+			abi::__cxa_demangle(name, nullptr, nullptr, &status), std::free};
+
+		return (status == 0) ? res.get() : name;
+#else
+		return name;
+#endif
+	}
+
+	template <typename T1, typename T2> auto typeCheck() -> bool
+	{
+		if (!std::is_base_of<T2, T2>())
+		{
+			es_coreError("Expected {}, got {}", demangle<T1>(), demangle<T2>());
+			return false;
+		}
+
+		return true;
+	}
+}
--- espresso/utils/threading/SystemManager.h
+++ espresso/utils/threading/SystemManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "utils/Demangle.h"
 #include "utils/memory/Ref.h"
 #include "utils/threading/System.h"
 #include <vector>
@@ -8,6 +9,33 @@ namespace Espresso::Threading
 	class SystemManager
 	{
 	public:
+		template <typename T> auto AddSystem() -> T*
+		{
+			if (Internals::typeCheck<System, T>())
+			{
+				return nullptr;
+			}
+
+			auto system = makeRef<T>();
+			_systems.push_back(system);
+		}
+
+		inline void Run()
+		{
+			for (auto& system : _systems)
+			{
+				system->Initialize();
+			}
+		}
+
+		inline void Shutdown()
+		{
+			for (auto& system : _systems)
+			{
+				system->Running.store(false);
+			}
+		}
+
 	private:
 		std::vector<Ref<System>> _systems;
 	};
